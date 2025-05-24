844867edf6aefb66f4bf4b83cdb774ca9c72d902
Elissa-DI <elissadusabe@gmail.com>
2025-05-24 21:35:46 +0200

    Initialized a proj

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,tsx,ts,jsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
